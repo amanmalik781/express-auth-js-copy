9115dd1953ae352ae26c97b3d3b2112e325856e4
AMAN DEEP MALIK <36561468+amanmalik781@users.noreply.github.com>
2024-04-18 18:27:32 +0530

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
+import dotenv from 'dotenv';
 import express from 'express';
-import http from 'http';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
@@ -7,6 +7,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import router from './router/index.js';
 
+dotenv.config();
 const app = express();
 
 app.use(cors({
@@ -17,16 +18,11 @@ app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-const server = http.createServer(app);
-
-
-const MONGO_URL = 'mongodb+srv://amandeepmalik:amandeepmalik@expressjsauth.aaqse1v.mongodb.net/authData?retryWrites=true&w=majority';
-mongoose.Promise = Promise;
-mongoose.connect(MONGO_URL);
-mongoose.connection.on('connected', () => {
-    console.log('mongo connected');
-    server.listen(8000, () => console.log('Server is running on port:8000'));
-});
-mongoose.connection.on('error', (error) => console.log('mongo connection error', error));
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log('mongo connected');
+        app.listen(process.env.PORT || 8000, () => console.log(`Server is running on port:${process.env.PORT || 8000}`));
+    })
+    .catch((error) => console.log('mongo connection error', error));
 
 app.use('/', router());
